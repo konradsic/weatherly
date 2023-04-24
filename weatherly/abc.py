121b35e4759301bf2b8c39b6063308dc733ff133
Konrad <sicinskikonrad@gmail.com>
2023-04-24 09:32:20 +0200

    Change WeatherAPIClient to Client, Forecast API responses in progress...

--- weatherly/abc.py
+++ weatherly/abc.py
@@ -222,78 +222,78 @@ class ForecastHourModel(ResponseModel):
 
     Attributes
     -------------
-        time_epoch: :class:`int`
-            Time as epoch
-        time: :class:`str`
-            Date and time
-        temp_c: :class:`float`
-            Temperature in celsius
-        temp_f: :class:`float`
-            Temperature in fahrenheit
-        condition_text: :class:`str`
-            Weather condition text
-        condition_icon: :class:`str`
-            Weather condition icons
-        condition_code: :class:`int`
-            Weather condition code
-        wind_mph: :class:`float`
-            Maximum wind speed in miles per hour
-        wind_kph: :class:`float`
-            Maximum wind speed in kilometer per hour
-        wind_degree: :class:`int`
-            Wind direction in degrees
-        wind_dir: :class:`str`
-            Wind direction as 16 point compass. e.g.: NSW
-        pressure_mb: :class:`float`
-            Pressure in millibars
-        pressure_in: :class:`float`
-            Pressure in inches
-        precip_mm: :class:`float`
-            Precipitation amount in millimeters
-        precip_in: :class:`float`
-            Precipitation amount in inches
-        humidity: :class:`int`
-            Humidity as percentage
-        cloud: :class:`int`
-            Cloud cover as percentage
-        feelslike_c: :class:`float`
-            Feels like temperature as celcius
-        feelslike_f: :class:`float`
-            Feels like temperature as fahrenheit
-        windchill_c: :class:`float`
-            Windchill temperature in celcius
-        windchill_f: :class:`float`
-            Windchill temperature in fahrenheit
-        headindex_c: :class:`float`
-            Heat index in celcius
-        headindex_f: :class:`float`
-            Heat index in fahrenheit
-        dewpoint_c: :class:`float`
-            Dew point in celcius
-        dewpoint_f: :class:`float`
-            Dew point in fahrenheit
-        will_it_rain: :class:`bool`
-            Will it will rain or not
-        will_it_snow: :class:`bool`
-            Will it will snow or not
-        is_day: :class:`bool`
-            Whether to show day condition icon or night icon
-        vis_km: :class:`float`
-            Visibility in kilometer
-        vis_miles: :class:`float`
-            Visibility in miles
-        chance_of_rain: :class:`int`
-            Chance of rain as percentage
-        chance_of_snow: :class:`int`
-            Chance of snow as percentage
-        gust_mph: :class:`float`
-            Wind gust in miles per hour
-        gust_kph: :class:`float`
-            Wind gust in kilometer per hour
-        uv: :class:`float`
-            UV Index
-        aqi: Optional[:class:`AirQuality`]
-            Air Quality data. See :class:`AirQualityData` for more info.
+    time_epoch: :class:`int`
+        Time as epoch
+    time: :class:`str`
+        Date and time
+    temp_c: :class:`float`
+        Temperature in celsius
+    temp_f: :class:`float`
+        Temperature in fahrenheit
+    condition_text: :class:`str`
+        Weather condition text
+    condition_icon: :class:`str`
+        Weather condition icons
+    condition_code: :class:`int`
+        Weather condition code
+    wind_mph: :class:`float`
+        Maximum wind speed in miles per hour
+    wind_kph: :class:`float`
+        Maximum wind speed in kilometer per hour
+    wind_degree: :class:`int`
+        Wind direction in degrees
+    wind_dir: :class:`str`
+        Wind direction as 16 point compass. e.g.: NSW
+    pressure_mb: :class:`float`
+        Pressure in millibars
+    pressure_in: :class:`float`
+        Pressure in inches
+    precip_mm: :class:`float`
+        Precipitation amount in millimeters
+    precip_in: :class:`float`
+        Precipitation amount in inches
+    humidity: :class:`int`
+        Humidity as percentage
+    cloud: :class:`int`
+        Cloud cover as percentage
+    feelslike_c: :class:`float`
+        Feels like temperature as celcius
+    feelslike_f: :class:`float`
+        Feels like temperature as fahrenheit
+    windchill_c: :class:`float`
+        Windchill temperature in celcius
+    windchill_f: :class:`float`
+        Windchill temperature in fahrenheit
+    headindex_c: :class:`float`
+        Heat index in celcius
+    headindex_f: :class:`float`
+        Heat index in fahrenheit
+    dewpoint_c: :class:`float`
+        Dew point in celcius
+    dewpoint_f: :class:`float`
+        Dew point in fahrenheit
+    will_it_rain: :class:`bool`
+        Will it will rain or not
+    will_it_snow: :class:`bool`
+        Will it will snow or not
+    is_day: :class:`bool`
+        Whether to show day condition icon or night icon
+    vis_km: :class:`float`
+        Visibility in kilometer
+    vis_miles: :class:`float`
+        Visibility in miles
+    chance_of_rain: :class:`int`
+        Chance of rain as percentage
+    chance_of_snow: :class:`int`
+        Chance of snow as percentage
+    gust_mph: :class:`float`
+        Wind gust in miles per hour
+    gust_kph: :class:`float`
+        Wind gust in kilometer per hour
+    uv: :class:`float`
+        UV Index
+    aqi: Optional[:class:`AirQuality`]
+        Air Quality data. See :class:`AirQualityData` for more info.
     """
     time_epoch: int
     time: str
@@ -343,60 +343,71 @@ class ForecastDayModel(ResponseModel):
 
     Attributes
     -------------
-        date: :class:`str`
-            
-        date_epoch: :class:`int`
-            
-        maxtemp_c: :class:`float`
-            
-        maxtemp_f: :class:`float`
-            
-        mintemp_c: :class:`float`
-            
-        mintemp_f: :class:`float`
-            
-        avgtemp_c: :class:`float`
-            
-        avgtemp_f: :class:`float`
-            
-        maxwind_mph: :class:`float`
-            
-        maxwind_mph: :class:`float`
-            
-        totalprecip_mm: :class:`float`
-            
-        totalprecip_in: :class:`float`
-            
-        avgvis_km: :class:`float`
-            
-        avgvis_miles: :class:`float`
-            
-        avghumidity: :class:`int`
-            
-        uv: :class:`int`
-            
-        condition_text: :class:`str`
-            
-        condition_icon: :class:`str`
-            
-        condition_code: :class:`int`
-            
-        sunrise: Optional[:class:`str`]
-            
-        sunset: Optional[:class:`str`]
-            
-        moonrise: Optional[:class:`str`]
-            
-        moonset: Optional[:class:`str`]
-            
-        moon_phase: Optional[:class:`str`]
-            
-        moon_illumination: Optional[:class:`float`]
-            
-        aqi: Optional[:class:`AirQuality`]
-            
-        hour_data: List[:class:`ForecastHourModel`]
-            
+    date: :class:`str`
+        Forecast date
+    date_epoch: :class:`int`
+        Forecast date as unix time.
+    maxtemp_c: :class:`float`
+        Maximum temperature in celsius for the day.
+    maxtemp_f: :class:`float`
+        Maximum temperature in fahrenheit for the day
+    mintemp_c: :class:`float`
+        Minimum temperature in celsius for the day
+    mintemp_f: :class:`float`
+        Minimum temperature in fahrenheit for the day
+    avgtemp_c: :class:`float`
+        Average temperature in celsius for the day
+    avgtemp_f: :class:`float`
+        Average temperature in fahrenheit for the day
+    maxwind_mph: :class:`float`
+        Maximum wind speed in miles per hour
+    maxwind_mph: :class:`float`
+        Maximum wind speed in kilometer per hour
+    totalprecip_mm: :class:`float`
+        Total precipitation in milimeter
+    totalprecip_in: :class:`float`
+        Total precipitation in inches
+    avgvis_km: :class:`float`
+        Average visibility in kilometer
+    avgvis_miles: :class:`float`
+        Average visibility in miles
+    avghumidity: :class:`int`
+        Average humidity as percentage
+    uv: :class:`int`
+        UV Index
+    condition_text: :class:`str`
+        Weather condition text
+    condition_icon: :class:`str`
+        Weather condition icon
+    condition_code: :class:`int`
+        Weather condition code
+    hour_data: List[:class:`ForecastHourModel`]
+        A list of :class:`ForecastHourModel` objects representing hourly weather data.
+        
+    .. note::
+        Below values can be ``None`` if user has disabled ``astro`` for astronomical data, ``aqi`` for air quality data.
+    sunrise: Optional[:class:`str`]
+        Sunrise time
+    sunset: Optional[:class:`str`]
+        Sunset time
+    moonrise: Optional[:class:`str`]
+        Moonrise time
+    moonset: Optional[:class:`str`]
+        Moonset time
+    moon_phase: Optional[:class:`str`]
+        Moon phases. Value returned:
+        * New Moon
+        * Waxing Crescent
+        * First Quarter
+        * Waxing Gibbous
+        * Full Moon
+        * Waning Gibbous
+        * Last Quarter
+        * Waning Crescent
+    moon_illumination: Optional[:class:`float`]
+        Moon illumination as %
+    aqi: Optional[:class:`AirQuality`]
+        Air Quality data as :class:`AirQuality` object.
     """
     date: str
     date_epoch: int
@@ -494,6 +505,12 @@ class ForecastModel(ResponseModel):
     
     Attributes
     -------------
+    location: :class:`LocationModel`
+        Location of the forecast data.
+    forecast_days: List[:class:`ForecastDayModel`]
+        A list of forecast days
+    alerts: List[:class:`AlertModel`]
+        A list of alerts, this list can be empty
     """
     location: LocationModel
     forecast_days: List[ForecastDayModel]
--- weatherly/responses.py
+++ weatherly/responses.py
@@ -22,17 +22,30 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
 
-from .abc import CurrentWeather, LocationModel, AirQuality
+from .abc import (
+    CurrentWeather, 
+    LocationModel, 
+    AirQuality, 
+    ForecastDayModel, 
+    ForecastHourModel, 
+    ForecastModel,
+    AlertModel,
+)
 from typing import (
     Dict,
     Any,
-    Union
+    Union,
+    Optional,
 )
 
 __all__ = (
     "CurrentWeatherData",
     "LocationData",
     "AirQualityData",
+    "ForecastData",
+    "ForecastDay",
+    "ForecastHour",
+    "AlertData",
 )
 
 GB_DEFRA_BAND = ("Low", "Low", "Low", "Moderate", "Moderate", "Moderate", "High", "High", "High", "Very High")
@@ -49,7 +62,7 @@ class CurrentWeatherData(CurrentWeather):
         Raw response in a JSON-like format (converted to a python dictionary)
     status: :class:`int`
         HTTP status of the response. 200 is OK, and is the most common status.
-    code: Union[:class:`int`, None]
+    code: Optional[:class:`int`]
         Response code. In some cases this can be ``None``
     location: :class:`LocationData`
         Location of the requested weather data
@@ -100,7 +113,7 @@ class CurrentWeatherData(CurrentWeather):
         self,
         raw: Dict[str, Any],
         status: int,
-        code: Union[int, None]
+        code: Optional[int]
     ) -> None:
         super().__init__()
         
@@ -143,7 +156,7 @@ class LocationData(LocationModel):
         Raw response in a JSON-like format (converted to a python dictionary)
     status: :class:`int`
         HTTP status of the response. 200 is OK, and is the most common status.
-    code: Union[:class:`int`, None]
+    code: Optional[:class:`int`]
         Response code. In some cases this can be ``None``
     id: Optional[:class:`int`]
         A specific ID of the location. Can be ``None``
@@ -168,7 +181,7 @@ class LocationData(LocationModel):
         self, 
         raw: Dict[str, Any],
         status: int,
-        code: Union[int, None]
+        code: Optional[int]
     ) -> None:
         self.raw = raw
         self.status = status
@@ -193,7 +206,7 @@ class AirQualityData(AirQuality):
         Raw response in a JSON-like format (converted to a python dictionary)
     status: :class:`int`
         HTTP status of the response. 200 is OK, and is the most common status.
-    code: Union[:class:`int`, None]
+    code: Optional[:class:`int`]
         Response code. In some cases this can be ``None``
     co: :class:`float`
         Carbon Monoxide (μg/m3)
@@ -232,7 +245,7 @@ class AirQualityData(AirQuality):
         self,
         raw: Dict[str, Any],
         status: int,
-        code: Union[int, None]
+        code: Optional[int]
     ) -> None:
         self.raw = raw
         self.status = status
@@ -248,3 +261,333 @@ class AirQualityData(AirQuality):
         self.gb_defra_index = raw["gb-defra-index"]
 
         self.gb_defra_band = GB_DEFRA_BAND[self.gb_defra_index-1]
+
+class AlertData(AlertModel):
+    """
+    A alert from WeatherAPI
+
+    Attributes
+    -------------
+    raw: Dict[:class:`str`, Any]
+        Raw response in a JSON-like format (converted to a python dictionary)
+    status: :class:`int`
+        HTTP status of the response. 200 is OK, and is the most common status.
+    code: Optional[:class:`int`]
+        Response code. In some cases this can be ``None``
+    headline: :class:`str`
+        Alert headline
+    msg_type: :class:`str`
+        Type of alert
+    severity: :class:`str`
+        Severity of alert
+    urgency: :class:`str`
+        Urgency
+    areas: :class:`str`
+        Areas covered
+    category: :class:`str`
+        Category
+    certainty: :class:`str`
+        Certainty
+    event: :class:`str`
+        Event
+    note: :class:`str`
+        Note
+    effective: :class:`str`
+        Effective
+    expires: :class:`str`
+        Expires
+    description: :class:`str`
+        Description
+    instruction: :class:`str`
+        Instruction
+    """
+    def __init__(
+        self,
+        raw: Dict[str, Any],
+        status: int,
+        code: Optional[int]
+    ) -> None:
+        self.raw = raw
+        self.status = status
+        self.code = code
+        
+        self.headline = raw["headline"]
+        self.msg_type = raw["msgType"]
+        self.severity = raw["severity"]
+        self.urgency = raw["urgency"]
+        self.areas = raw["areas"]
+        self.category = raw["category"]
+        self.certainty = raw["certainty"]
+        self.event = raw["event"]
+        self.note = raw["note"]
+        self.effective = raw["effective"]
+        self.expires = raw["expires"]
+        self.description = raw["desc"]
+        self.instruction = raw["instruction"]
+
+class ForecastHour(ForecastHourModel):
+    """
+    Forecast hour, an element of :class:`ForecastDay`
+    
+    Attributes
+    -------------
+    raw: Dict[:class:`str`, Any]
+        Raw response in a JSON-like format (converted to a python dictionary)
+    status: :class:`int`
+        HTTP status of the response. 200 is OK, and is the most common status.
+    code: Optional[:class:`int`]
+        Response code. In some cases this can be ``None``
+    time_epoch: :class:`int`
+        Time as epoch
+    time: :class:`str`
+        Date and time
+    temp_c: :class:`float`
+        Temperature in celsius
+    temp_f: :class:`float`
+        Temperature in fahrenheit
+    condition_text: :class:`str`
+        Weather condition text
+    condition_icon: :class:`str`
+        Weather condition icons
+    condition_code: :class:`int`
+        Weather condition code
+    wind_mph: :class:`float`
+        Maximum wind speed in miles per hour
+    wind_kph: :class:`float`
+        Maximum wind speed in kilometer per hour
+    wind_degree: :class:`int`
+        Wind direction in degrees
+    wind_dir: :class:`str`
+        Wind direction as 16 point compass. e.g.: NSW
+    pressure_mb: :class:`float`
+        Pressure in millibars
+    pressure_in: :class:`float`
+        Pressure in inches
+    precip_mm: :class:`float`
+        Precipitation amount in millimeters
+    precip_in: :class:`float`
+        Precipitation amount in inches
+    humidity: :class:`int`
+        Humidity as percentage
+    cloud: :class:`int`
+        Cloud cover as percentage
+    feelslike_c: :class:`float`
+        Feels like temperature as celcius
+    feelslike_f: :class:`float`
+        Feels like temperature as fahrenheit
+    windchill_c: :class:`float`
+        Windchill temperature in celcius
+    windchill_f: :class:`float`
+        Windchill temperature in fahrenheit
+    headindex_c: :class:`float`
+        Heat index in celcius
+    headindex_f: :class:`float`
+        Heat index in fahrenheit
+    dewpoint_c: :class:`float`
+        Dew point in celcius
+    dewpoint_f: :class:`float`
+        Dew point in fahrenheit
+    will_it_rain: :class:`bool`
+        Will it will rain or not
+    will_it_snow: :class:`bool`
+        Will it will snow or not
+    is_day: :class:`bool`
+        Whether to show day condition icon or night icon
+    vis_km: :class:`float`
+        Visibility in kilometer
+    vis_miles: :class:`float`
+        Visibility in miles
+    chance_of_rain: :class:`int`
+        Chance of rain as percentage
+    chance_of_snow: :class:`int`
+        Chance of snow as percentage
+    gust_mph: :class:`float`
+        Wind gust in miles per hour
+    gust_kph: :class:`float`
+        Wind gust in kilometer per hour
+    uv: :class:`float`
+        UV Index
+    aqi: Optional[:class:`AirQualityData`]
+        Air Quality data. See :class:`AirQualityData` for more info.
+    """
+    def __init__(
+        self,
+        raw: Dict[str, Any],
+        status: int,
+        code: Optional[int]
+    ) -> None:
+        self.raw = raw
+        self.status = status
+        self.code = code
+    
+class ForecastDay(ForecastDayModel):
+    """
+    A forecast day, element of :class:`ForecastData`
+
+    Attributes
+    -------------
+    raw: Dict[:class:`str`, Any]
+        Raw response in a JSON-like format (converted to a python dictionary)
+    status: :class:`int`
+        HTTP status of the response. 200 is OK, and is the most common status.
+    code: Optional[:class:`int`]
+        Response code. In some cases this can be ``None``
+    date: :class:`str`
+        Forecast date
+    date_epoch: :class:`int`
+        Forecast date as unix time.
+    maxtemp_c: :class:`float`
+        Maximum temperature in celsius for the day.
+    maxtemp_f: :class:`float`
+        Maximum temperature in fahrenheit for the day
+    mintemp_c: :class:`float`
+        Minimum temperature in celsius for the day
+    mintemp_f: :class:`float`
+        Minimum temperature in fahrenheit for the day
+    avgtemp_c: :class:`float`
+        Average temperature in celsius for the day
+    avgtemp_f: :class:`float`
+        Average temperature in fahrenheit for the day
+    maxwind_mph: :class:`float`
+        Maximum wind speed in miles per hour
+    maxwind_mph: :class:`float`
+        Maximum wind speed in kilometer per hour
+    totalprecip_mm: :class:`float`
+        Total precipitation in milimeter
+    totalprecip_in: :class:`float`
+        Total precipitation in inches
+    avgvis_km: :class:`float`
+        Average visibility in kilometer
+    avgvis_miles: :class:`float`
+        Average visibility in miles
+    avghumidity: :class:`int`
+        Average humidity as percentage
+    uv: :class:`int`
+        UV Index
+    condition_text: :class:`str`
+        Weather condition text
+    condition_icon: :class:`str`
+        Weather condition icon
+    condition_code: :class:`int`
+        Weather condition code
+    hour_data: List[:class:`ForecastHour`]
+        A list of :class:`ForecastHour` objects representing hourly weather data.
+        
+    .. note::
+        Below values can be ``None`` if user has disabled ``astro`` for astronomical data, ``aqi`` for air quality data.
+    sunrise: Optional[:class:`str`]
+        Sunrise time
+    sunset: Optional[:class:`str`]
+        Sunset time
+    moonrise: Optional[:class:`str`]
+        Moonrise time
+    moonset: Optional[:class:`str`]
+        Moonset time
+    moon_phase: Optional[:class:`str`]
+        Moon phases. Value returned:
+        * New Moon
+        * Waxing Crescent
+        * First Quarter
+        * Waxing Gibbous
+        * Full Moon
+        * Waning Gibbous
+        * Last Quarter
+        * Waning Crescent
+    moon_illumination: Optional[:class:`float`]
+        Moon illumination as %
+    aqi: Optional[:class:`AirQualityData`]
+        Air Quality data as :class:`AirQualityData` object.
+    """
+    def __init__(
+        self,
+        raw: Dict[str, Any],
+        status: int,
+        code: Optional[int]
+    ) -> None:
+        self.raw = raw
+        self.status = status
+        self.code = code
+        
+        before_raw = raw.copy()
+        raw = raw["day"]
+        
+        self.date = raw["date"]
+        self.date_epoch = raw["date_epoch"]
+        self.maxtemp_c = raw["maxtemp_c"]
+        self.maxtemp_f = raw["maxtemp_f"]
+        self.mintemp_c = raw["mintemp_c"]
+        self.mintemp_f = raw["mintemp_f"]
+        self.avgtemp_c = raw["avgtemp_c"]
+        self.avgtemp_f = raw["avgtemp_f"]
+        self.maxwind_mph = raw["maxwind_mph"]
+        self.maxwind_kph = raw["maxwind_kph"]
+        self.totalprecip_in = raw["totalprecip_in"]
+        self.totalprecip_mm = raw["totalprecip_mm"]
+        self.avgvis_km = raw["avgvis_km"]
+        self.avgvis_miles = raw["avgvis_miles"]
+        self.avghumidity = raw["avghumidity"]
+        self.uv = raw["uv"]
+        self.condition_text = raw["condition"]["text"]
+        self.condition_icon = raw["condition"]["icon"]
+        self.condition_code = raw["condition"]["code"]
+        
+        raw = before_raw
+        self.hour_data = list([
+            ForecastHour(elem, status, code) for elem in raw["hour"]
+        ])
+        
+        if raw.get("astro", None):
+            self.sunrise = raw["astro"]["sunrise"]
+            self.sunset = raw["astro"]["sunset"]
+            self.moonrise = raw["astro"]["moonrise"]
+            self.moonset = raw["astro"]["moonset"]
+            self.moon_phase = raw["astro"]["moon_phase"]
+            self.moon_illumination = raw["astro"]["moon_illumination"]
+        else:
+            self.sunrise = None
+            self.sunset = None
+            self.moonrise = None
+            self.moonset = None
+            self.moon_phase = None
+            self.moon_illumination = None
+    
+class ForecastData(ForecastModel):
+    """
+    Forecast data returned from Forecast API
+    
+    Attributes
+    -------------
+    raw: Dict[:class:`str`, Any]
+        Raw response in a JSON-like format (converted to a python dictionary)
+    status: :class:`int`
+        HTTP status of the response. 200 is OK, and is the most common status.
+    code: Optional[:class:`int`]
+        Response code. In some cases this can be ``None``
+    location: :class:`LocationData`
+        Location of the forecast data.
+    forecast_days: List[:class:`ForecastDay`]
+        A list of forecast days
+    alerts: List[:class:`AlertData`]
+        A list of alerts, this list can be empty. List is also empty, when the user disabled alerts in the request.
+    """
+    def __init__(
+        self,
+        raw: Dict[str, Any],
+        status: int,
+        code: Optional[int]
+    ) -> None:
+        self.raw = raw
+        self.status = status
+        self.code = code
+        
+        self.location = LocationData(raw["location"], status, code)
+        self.forecast_days = list([ForecastDay(elem, status, code) for elem in raw["forecast"]["forecastday"]])
+        self.alerts = []
+        alert_data = raw.get("alerts", {})
+        
+        for k,v in alert_data.items():
+            if v is not []:
+                self.alerts.extend(list([
+                    AlertData(elem, status, code) for elem in v
+                ]))
+
